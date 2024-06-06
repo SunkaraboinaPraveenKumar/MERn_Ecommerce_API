2cb90a2443931a7323c0b6c84e75c7069581345a
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-06-07 02:11:22 +0530

    Changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json())
 
 app.use(cors({
-  origin:true,
+  origin:'https://mern-ecommerce-api1.onrender.com/api',
   methods:["GET","POST","PUT","DELETE"],
   credentials:true
 }))
