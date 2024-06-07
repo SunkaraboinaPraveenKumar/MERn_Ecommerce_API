c9dab423b332f228db7b0dcffd06cbc625d12caf
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-06-07 09:49:00 +0530

    Last Changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json())
 
 app.use(cors({
-  origin:true,
+  origin:'https://merne-commerce-praveen-6xi1.vercel.app/',
   methods:["GET","POST","PUT","DELETE"],
   credentials:true
 }))
